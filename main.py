071efe1193a03ff0ea69cd9bd38892e844f55ad4
Pranjal Jain <35431370+pranjal0000@users.noreply.github.com>
2019-10-30 17:38:23 +0530

    Update main.py

--- main.py
+++ main.py
@@ -8,24 +8,24 @@ def main():
 	pass
 
 @main.command()
-@click.option('-m', '--model', help='fullConn' , required=False)
+@click.option('-m', '--model', help='UNet' , required=False)
 def train(model):
     pipeline_manager.train(model)
 
 @main.command()
-@click.option('-m', '--model', help='fullConn' , required=False)
+@click.option('-m', '--model', help='Unet' , required=False)
 def test(model):
     pipeline_manager.test(model)
 
 @main.command()
-@click.option('-m', '--model', help='fullConn' , required=False)
+@click.option('-m', '--model', help='UNet' , required=False)
 @click.option('-p', '--path', help='MP3 Path' , required=True)
 @click.option('-o', '--out_path', help='Save Path' , required=True)
 def test_one(model, path, out_path):
     pipeline_manager.test_one(model, path, out_path)
 
 @main.command()
-@click.option('-m', '--model', help='fullConn' , required=False)
+@click.option('-m', '--model', help='UNet' , required=False)
 @click.option('-p', '--path', help='MP3 Path' , required=False)
 @click.option('-o', '--out_path', help='Save Path' , required=False)
 def test_entire_folder(model, path, out_path):
