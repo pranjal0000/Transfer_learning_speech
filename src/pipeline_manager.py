692fa4201c2fd7f406ceeb95adbedaa1fc9a53b1
Pranjal Jain <35431370+pranjal0000@users.noreply.github.com>
2019-11-05 22:21:55 +0530

    Update pipeline_manager.py
    updated

--- src/pipeline_manager.py
+++ src/pipeline_manager.py
@@ -19,18 +19,18 @@ class PipelineManager():
 	def test(self, model_name):
 		test(model_name)
 
-	def test_one(self, model_name, path, out_path):
-		test_one_image(model_name, path, out_path)
+	# def test_one(self, model_name, path, out_path):
+	# 	test_one_image(model_name, path, out_path)
 
-	def test_entire_folder(self, model_name, path='/home/mayank/Desktop/GitRepos/Text/Dataset/Passports_Dataset', out_path='/home/mayank/Desktop/GitRepos/Text/Dataset/Passports_Dataset_output'):
-		test_entire_folder(model_name, path, out_path)
+	# def test_entire_folder(self, model_name, path, out_path):
+	# 	test_entire_folder(model_name, path, out_path)
 
 def train(model_name):
 
 	from dl_model import dl_model
 	
 	if model_name == 'UNet' or model_name == None:
-		driver = dl_model('UNet')
+		driver = dl_model('UNet', mode = 'train')
 		driver.train_model()
 	else:
 		log.info("Not yet implemented")
@@ -45,25 +45,25 @@ def test(model_name):
 	else:
 		print("Not yet implemented")
 
-def test_one_image( model_name, path, out_path):
+# def test_one_image( model_name, path, out_path):
 
-	from dl_model import dl_model
+# 	from dl_model import dl_model
 
-	if model_name == 'UNet' or model_name == None:
-		driver = dl_model('UNet', mode='test_one')
-		driver.test_one_image(path, out_path)
-	else:
-		print("Not yet implemented")
+# 	if model_name == 'UNet' or model_name == None:
+# 		driver = dl_model('UNet', mode='test_one')
+# 		driver.test_one_image(path, out_path)
+# 	else:
+# 		print("Not yet implemented")
 
-def test_entire_folder(model_name, path, out_path):
+# def test_entire_folder(model_name, path, out_path):
 
-	print(path, out_path)
+# 	print(path, out_path)
 
-	from dl_model import dl_model
+# 	from dl_model import dl_model
 
-	if model_name == 'UNet':
+# 	if model_name == 'UNet':
 
-		if not os.path.exists(out_path):
-			os.mkdir(out_path)
-		driver = dl_model(model_name, mode='test_one')
-		gen(driver, path, out_path)
+# 		if not os.path.exists(out_path):
+# 			os.mkdir(out_path)
+# 		driver = dl_model(model_name, mode='test_one')
+# 		gen(driver, path, out_path)
